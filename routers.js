8164821f24b5c22be8eda8d028fd13363d3e65fe
Frederick369 <frederick.ngo@rogers.com>
2015-11-22 16:43:40 -0500

    First

---
+++ routers.js
@@ -0,0 +1,16 @@
+Router.route('/',{
+    template:'home',
+    title: "MGCI Math Society - Home Page"
+});
+Router.route('/about',{
+    template:'about',
+    title: "MGCI Math Society - About"
+});
+Router.route('/mgmt',{
+    template:'mgmt',
+    title:"MGMT"
+});
+Router.route('/contact',{
+    template:'contact',
+    title: "MGCI Math Society - Contact Us"
+});
