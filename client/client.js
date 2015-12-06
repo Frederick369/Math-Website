8212a5b3a3dce85b2dd9d1f6e4ed64e2e80608eb
Frederick369 <frederick.ngo@rogers.com>
2015-12-06 16:28:11 -0500

    Updated website

--- client/client.js
+++ client/client.js
@@ -1,6 +1,8 @@
+
 if (Meteor.isClient){
-    Template.landing.rendered = new WOW().init();
+    Template.home.rendered = new WOW().init();
 }
+
 Template.loginButtons.rendered = function()
 {
     Accounts._loginButtonsSession.set('dropdownVisible', true);
--- client/routers.js
+++ client/routers.js
@@ -1,24 +1,36 @@
-Router.route('/',{
-    template:'home',
+Router.route('/', {
+    template: 'home',
     title: "MGCI Math Society - Home Page"
 });
-Router.route('/about',{
-    template:'about',
+Router.route('/about', {
+    template: 'about',
     title: "MGCI Math Society - About"
 });
-Router.route('/mgmt',{
-    template:'mgmt',
-    title:"MGMT"
+Router.route('/mgmt', {
+    template: 'mgmt',
+    title: "MGMT"
 });
-Router.route('/contact',{
-    template:'contact',
+Router.route('/contact', {
+    template: 'contact',
     title: "MGCI Math Society - Contact Us"
 });
-Router.route('/date',{
-    template:'contestdates',
+Router.route('/date', {
+    template: 'contestdates',
     title: "MGCI Math Society - Contest Dates"
-})
-Router.route('/login',{
-    template:'login',
+});
+Router.route('/login', {
+    template: 'login',
     title: "MGCI Math Society - Login"
-})
+});
+Router.route('/javascript', {
+    template: 'javascript',
+    title: "MGCI Math Society - Javascript"
+    /*onBeforeACtion: function (){
+    var currentUser = Meteor.userID();
+    if (currentUser){
+    this.render('javascript');
+    }
+    else{
+    this.render('login');    }
+}*/
+});
